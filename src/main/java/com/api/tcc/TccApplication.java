ba03820dcdfca7b0aaed66a47f18589b5b5360a2
Mateus Sampaio <mateussam.souza@gmail.com>
2020-09-09 02:08:59 -0300

    Merge Dev into Master (#19)
    * Add gitignore file
    
    * Change readme
    
    * Add Eclipse IDE in gitignore itens (#5)
    
    * Add Eclipse IDE in gitignore file (#8)
    
    * Merge with master
    
    * Add gitignore file
    
    * Change readme
    
    * Add Eclipse IDE in gitignore itens
    
    * Refactor/azure ci pipeline (#11)
    
    * Dev (#9)
    
    * Add gitignore file
    
    * Change readme
    
    * Add Eclipse IDE in gitignore itens (#5)
    
    * Add Eclipse IDE in gitignore file (#8)
    
    * Merge with master
    
    * Add gitignore file
    
    * Change readme
    
    * Add Eclipse IDE in gitignore itens
    
    * Set up CI with Azure Pipelines
    
    [skip ci]
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Add ignore test failure in pom.xml
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update README file (#13)
    
    * Change properties values to environments variables (#17)
    
    * Update azure-pipelines.yml for Azure Pipelines (#18)

--- src/main/java/com/api/tcc/TccApplication.java
+++ src/main/java/com/api/tcc/TccApplication.java
@@ -1,13 +1,18 @@
 package com.api.tcc;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan(basePackageClasses = { JsonProperties.class })
 public class TccApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TccApplication.class, args);
+		SpringApplication app = new SpringApplication(TccApplication.class);
+		app.addInitializers(new JsonPropertyContextInitializer());
+		app.run(args);
 	}
-
 }
