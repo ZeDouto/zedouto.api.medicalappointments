644a77ea0509e2bef434eb3587518cced7d5a7ec
souzaMateus99 <mateussam.souza@gmail.com>
2020-09-16 21:35:38 -0300

    Test

--- src/main/java/com/api/tcc/adapter/http/routes/Routes.java
+++ src/main/java/com/api/tcc/adapter/http/routes/Routes.java
@@ -7,4 +7,5 @@ public class Routes {
 	public static final String ROUTE_PACIENTE = CONTEXTO_API + "/pacientes";
 	public static final String ROUTE_HOSPITAL = CONTEXTO_API + "/hospitais";
 	public static final String ROUTE_CONSULTA = CONTEXTO_API + "/consultas";
+	public static final String ROUTE_HEALTH_CHECK = CONTEXTO_API + "/healthz";
 }
