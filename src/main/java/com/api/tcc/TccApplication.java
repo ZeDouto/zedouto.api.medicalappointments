1001b6af8460ebefd1784ea91c13854d5d8d63f9
Mateus Sampaio <mateussam.souza@gmail.com>
2020-09-20 18:06:46 -0300

    Containerize application (#28)
    * Create dockerfile and docker-compose files
    
    * Remove configuration by AppConfig.json
    
    * Set up CI with Azure Pipelines
    
    Change azure pipelines file to use Dockerfile [skip ci]
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Remove database validate
    
    * Add database validate

--- src/main/java/com/api/tcc/TccApplication.java
+++ src/main/java/com/api/tcc/TccApplication.java
@@ -1,18 +1,12 @@
 package com.api.tcc;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@EnableAutoConfiguration
-@ComponentScan(basePackageClasses = { JsonProperties.class })
 public class TccApplication {
 
 	public static void main(String[] args) {
-		SpringApplication app = new SpringApplication(TccApplication.class);
-		app.addInitializers(new JsonPropertyContextInitializer());
-		app.run(args);
+		SpringApplication.run(TccApplication.class, args);
 	}
 }
