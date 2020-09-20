023f0bc8712e08130fbb7e3b634f28b40e75891e
Mateus Sampaio <mateussam.souza@gmail.com>
2020-09-20 18:45:09 -0300

    Dev (#31)
    * Add gitignore file
    
    * Change readme
    
    * Add Eclipse IDE in gitignore itens (#5)
    
    * Add Eclipse IDE in gitignore file (#8)
    
    * Merge with master
    
    * Add gitignore file
    
    * Change readme
    
    * Add Eclipse IDE in gitignore itens
    
    * Refactor/azure ci pipeline (#11)
    
    * Dev (#9)
    
    * Add gitignore file
    
    * Change readme
    
    * Add Eclipse IDE in gitignore itens (#5)
    
    * Add Eclipse IDE in gitignore file (#8)
    
    * Merge with master
    
    * Add gitignore file
    
    * Change readme
    
    * Add Eclipse IDE in gitignore itens
    
    * Set up CI with Azure Pipelines
    
    [skip ci]
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Add ignore test failure in pom.xml
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update README file (#13)
    
    * Change properties values to environments variables (#17)
    
    * Update azure-pipelines.yml for Azure Pipelines (#18)
    
    * Update azure-pipelines.yml for Azure Pipelines (#20)
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines (#22)
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Change values in AppConfig file to token (#24)
    
    * Add the token to replace values in deployment
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Add the properties infos
    
    * Remove the parameter to create database if not exists
    
    * Containerize application (#28)
    
    * Create dockerfile and docker-compose files
    
    * Remove configuration by AppConfig.json
    
    * Set up CI with Azure Pipelines
    
    Change azure pipelines file to use Dockerfile [skip ci]
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Remove database validate
    
    * Add database validate
    
    * Add cors validation (accept any origins) (#29)
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Change config stage
    
    * Add stage prop
    
    * Add configuration annotation

--- src/main/java/com/api/tcc/adapter/http/config/WebConfig.java
+++ src/main/java/com/api/tcc/adapter/http/config/WebConfig.java
@@ -3,6 +3,8 @@ package com.api.tcc.adapter.http.config;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
