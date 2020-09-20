a1d63cc8fe39941c624d1857911af3dba8e9669e
Mateus Sampaio <mateussam.souza@gmail.com>
2020-09-20 18:12:59 -0300

    Add cors validation (accept any origins) (#29)

---
+++ src/main/java/com/api/tcc/adapter/http/config/WebConfig.java
@@ -0,0 +1,15 @@
+package com.api.tcc.adapter.http.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry
+            .addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("*")
+            .allowedHeaders("*");
+    }
+}
