eb5061c101c246a8d9b7ca6c2429047199171022
souzaMateus99 <mateussam.souza@gmail.com>
2020-10-01 00:42:16 -0300

    Create request to return 'Consulta' by patient cpf

--- src/main/java/com/api/tcc/core/service/ConsultaService.java
+++ src/main/java/com/api/tcc/core/service/ConsultaService.java
@@ -3,6 +3,9 @@ package com.api.tcc.core.service;
 import java.util.List;
 import java.util.UUID;
 
+import com.api.tcc.adapter.entity.PacienteEntity;
+import com.api.tcc.adapter.mapper.PacienteMapper;
+import com.api.tcc.core.model.Paciente;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,8 +24,14 @@ public class ConsultaService {
 		return ConsultaMapper.converterDTO(repository.save(ConsultaMapper.converterEntity(model)));
 	}
 	
-	public List<Consulta> getAll(){
+	public List<Consulta> getAll() {
 		return ConsultaMapper.converterListaDTO(repository.findAll());
 	}
 
+	public List<Consulta> getByPaciente(String cpf) {
+		Paciente paciente = new Paciente();
+		paciente.setCpf(cpf);
+
+		return ConsultaMapper.converterListaDTO(repository.findByPaciente(PacienteMapper.converterEntity(paciente)).orElse(null));
+	}
 }
