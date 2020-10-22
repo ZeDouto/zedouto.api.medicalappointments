b8a27d58b590f6e9577d1187f5b881b8d19d376c
paivam <matheussilvapaiva@hotmail.com>
2020-10-22 01:32:05 -0300

    get id consulta

--- src/main/java/com/api/tcc/adapter/http/ConsultaController.java
+++ src/main/java/com/api/tcc/adapter/http/ConsultaController.java
@@ -8,7 +8,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.api.tcc.adapter.http.routes.Routes;
 import com.api.tcc.core.model.Consulta;
@@ -31,9 +37,19 @@ public class ConsultaController {
 	public ResponseEntity<List<Consulta>> get() {
 		return new ResponseEntity<>(service.getAll(), HttpStatus.OK);
 	}
+	
+	@GetMapping(value = Routes.PATH_ID)
+	public ResponseEntity<Consulta> getId(@PathVariable(name = "id") String id) {
+		return new ResponseEntity<>(service.getId(id), HttpStatus.OK);
+	}
 
-	@GetMapping(value = Routes.PATH_CPF)
+	@GetMapping(value = Routes.PATH_CONSULTA_BY_PACIENTE)
 	public ResponseEntity<List<Consulta>> getByPaciente(@PathVariable(name = "cpf") String cpf) {
 		return new ResponseEntity<>(service.getByPaciente(cpf), HttpStatus.OK);
+	}
+
+	@GetMapping(value = Routes.PATH_CONSULTA_BY_MEDICO)
+	public ResponseEntity<List<Consulta>> getByMedico(@PathVariable(name = "cpf") String cpf) {
+		return new ResponseEntity<>(service.getByMedico(cpf), HttpStatus.OK);
 	}
 }
