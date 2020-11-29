d04d33a3dab29c97b7bbadb586a0f2c0ec867dca
souzaMateus99 <mateussam.souza@gmail.com>
2020-11-29 20:24:16 -0300

    Change the id type to int

--- src/main/java/com/api/tcc/adapter/HospitalMedicoRepository.java
+++ src/main/java/com/api/tcc/adapter/HospitalMedicoRepository.java
@@ -3,10 +3,12 @@ package com.api.tcc.adapter;
 import com.api.tcc.adapter.entity.HospitalEntity;
 import com.api.tcc.adapter.entity.HospitalMedicoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface HospitalMedicoRepository extends JpaRepository<HospitalMedicoEntity, String> {
     List<HospitalMedicoEntity> findByHospital(HospitalEntity hospital);
 }
